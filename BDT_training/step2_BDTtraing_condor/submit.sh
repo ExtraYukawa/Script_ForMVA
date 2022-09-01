bef5cd18550a6be63e8c775079ddca22a707fea5
Gouranga Kole <gouranga.tifr@gmail.com>
2022-09-01 19:57:50 +0200

    Color shell output

--- BDT_training/step2_BDTtraing_condor/submit.sh
+++ BDT_training/step2_BDTtraing_condor/submit.sh
@@ -1,7 +1,9 @@
 PWd=`pwd`
 cps=("rtc01" "rtu01")
-mass=(200) # 300 350 400 500 600 700)
-era=("2016apv") # "2016postapv" "2017" "2018")
+mass=(200 700) # 300 350 400 500 600 700)
+#era=("2016apv" "2016postapv" "2017" "2018")
+#era=("2016postapv") # "2017" "2018")
+era=("2018")
 
 #for TA submission
 for k in ${era[*]}
@@ -12,6 +14,7 @@ do
 	do
 	cd ${k}/ttc_a_${j}_MA$i
 	echo $PWD
+	echo -e "\033[0;32m Submitting for ttc_a_${j}_MA$i in ${k} era \033[0m"
 	condor_submit sub.jdl
 	cd $PWd
 	done
@@ -27,6 +30,7 @@ do
 	do
 	cd ${k}/ttc_s0_${j}_MS0$i
 	echo $PWD
+	echo -e "\033[0;32m Submitting for ttc_s0_${j}_MS0$i in ${k} era \033[0m"
 	condor_submit sub.jdl
 	cd $PWd
 	done

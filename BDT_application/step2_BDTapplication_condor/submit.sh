75a810d5c9ba8ac45f53a9ed9c2e9c7e3d21de73
Gouranga Kole <gouranga.tifr@gmail.com>
2022-09-08 17:33:36 +0200

    Condor job submission script

--- BDT_application/step2_BDTapplication_condor/submit.sh
+++ BDT_application/step2_BDTapplication_condor/submit.sh
@@ -1,27 +1,39 @@
 PWd=`pwd`
 cps=("rtc01" "rtc04" "rtc08" "rtc10" "rtu01" "rtu04" "rtu08" "rtu10")
-mass=(200 300 350 400 500 600 700)
+#cps=("rtc01") # "rtc04" "rtc08" "rtc10" "rtu01" "rtu04" "rtu08" "rtu10")
+#mass=(200 300 350 400 500 600 700)
+mass=(600 700) # 300 350 400 500 600 700)
+#era=("2016apv" "2016postapv" "2017" "2018")
+era=("2016postapv")
 
 #for TA submission
-for j in ${cps[*]}
+for k in ${era[*]}
 do
-  for i in ${mass[*]}
-  do
-  cd ttc_a_${j}_MA$i
-  echo $PWD
-  condor_submit sub.jdl
-  cd $PWd
-  done
+    for j in ${cps[*]}
+    do
+	for i in ${mass[*]}
+	do
+	cd ${k}/ttc_a_${j}_MA$i
+	echo $PWD
+	echo -e "\033[0;32m Evaluating for ttc_a_${j}_MA$i in ${k} era \033[0m"
+	condor_submit sub.jdl
+	cd $PWd
+	done
+    done
 done
 
 #for TS0 submission
-for j in ${cps[*]}
+for k in ${era[*]}
 do
-  for i in ${mass[*]}
-  do
-  cd ttc_s0_${j}_MS0$i
-  echo $PWD
-  condor_submit sub.jdl
-  cd $PWd
-  done
+    for j in ${cps[*]}
+    do
+	for i in ${mass[*]}
+	do
+	cd ${k}/ttc_s0_${j}_MS0$i
+	echo $PWD
+	echo -e "\033[0;32m Evaluating for ttc_s0_${j}_MS0$i in ${k} era \033[0m"
+	condor_submit sub.jdl
+	cd $PWd
+	done
+    done
 done

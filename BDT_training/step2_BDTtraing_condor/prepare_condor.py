49061aa6406253f11cc07205c3afd2fc7e39784b
AlbertHsuNTUphys <b07202037@ntu.edu.tw>
2022-09-13 09:14:32 +0200

    update BDT_training

--- BDT_training/step2_BDTtraing_condor/prepare_condor.py
+++ BDT_training/step2_BDTtraing_condor/prepare_condor.py
@@ -3,6 +3,9 @@ import sys
 from os import walk
 import optparse
 import subprocess
+from collections import OrderedDict
+
+cmsswBase = os.environ['CMSSW_BASE']
 
 def prepare_condor(sample,coupling,mass):
   WHICH_SAMPLE=sample
@@ -15,6 +18,32 @@ def prepare_condor(sample,coupling,mass):
 
   sample=SIGNAL
   return sample
+
+def GetBackgroundList(era):
+  jsonfile = open(os.path.join(cmsswBase + '/src/Script_ForMVA/data/sample_' + str(era) + 'UL.json'))
+  samples  = json.load(jsonfile, encoding='utf-8', object_pairs_hook=OrderedDict).items()
+  jsonfile.close()
+  BackgroundFile_List = []
+  for process, desc in samples:
+    if desc[5] == 1:
+      BackgroundFile_List.append((str((process + ".root")),desc[0]))
+  return BackgroundFile_List
+
+def prepare_BackgroundCommand(era):
+  
+  Background_List = GetBackgroundList(era)
+  count = 1
+  init_input = "\\n"
+  backgroundfile_input = "\\n"
+  backgroundfile_readtree = "\\n"
+  addbackgroundtree = "\\n"
+  for f in Background_List:
+    init_input += "   TFile *input_B%d(0);\\n"%(count)
+    backgroundfile_input += '   input_B%d=TFile::Open(\\"%s\\");\\n'%(count, f[0])
+    backgroundfile_readtree += '   TTree *background%d     = (TTree*)input_B%d->Get(\\"SlimTree\\");\\n'%(count,count)
+    addbackgroundtree += "   dataloader->AddBackgroundTree( background%d,%f\/background%d->GetEntries());\\n"%(count,f[1],count)
+    count += 1
+  return init_input, backgroundfile_input, backgroundfile_readtree, addbackgroundtree
 
 if __name__ == "__main__":
   PWD=os.getcwd()
@@ -40,12 +69,16 @@ if __name__ == "__main__":
     masses=['200', '700'] #,'300','350','400','500','600','700'] #fixme gkole
     system_unc=["central","jesup","jesdo","jerup","jerdo","metUnslusEnup","metUnslusEndo"]
 
+    init_input, backgroundfile_input, backgroundfile_readtree, addbackgroundtree = prepare_BackgroundCommand(Era)
+    print(init_input)
+
     for isig in range(0,len(signals)):
       for ic in range(0,len(couplings)):
         for im in range(0,len(masses)):
           samples_temp=prepare_condor(signals[isig],couplings[ic],masses[im])
           dir_temp=samples_temp.split('.')[0].split('/')[-1]
           print ("dir_temp: ", dir_temp)
+          os.system('rm -rf %s/%s'%(Era,dir_temp))
           os.mkdir(Era+'/'+dir_temp)
           os.chdir(Era+'/'+dir_temp)
           print ("cwd: ", os. getcwd())
@@ -57,10 +90,11 @@ if __name__ == "__main__":
             os.system(r'sed -i "s/SIGNALROOT/%s/g" wrapper_%s.sh' %(samples_temp, Era))
             os.system(r'sed -i "s/YEAR/%s/g" wrapper_%s.sh' %(Era, Era))
             os.system(r'sed -i "s/TMVAClassification.C/TMVAClassification_%s.C/g" wrapper_%s.sh' %(Era, Era))
-            if Era == "2016apv" or Era == "2016postapv":
-              os.system(r'cp ../../../TMVAClassification_%s.C .' %(Era))
-            else:
-              os.system(r'cp ../../../TMVAClassification.C TMVAClassification_%s.C' %(Era))
+            os.system(r'cp ../../../../../script/TMVAClassification.C TMVAClassification_%s.C' %(Era))
+            os.system(r'sed -i "s/INIT_INPUT/%s/g" TMVAClassification_%s.C' %(init_input,Era))
+            os.system(r'sed -i "s/BACKGROUNDFILE_INPUT/%s/g" TMVAClassification_%s.C'%(backgroundfile_input,Era))
+            os.system(r'sed -i "s/BACKGROUNDFILE_READTREE/%s/g" TMVAClassification_%s.C'%(backgroundfile_readtree,Era))
+            os.system(r'sed -i "s/ADDBACKGROUNDTREE/%s/g" TMVAClassification_%s.C'%(addbackgroundtree,Era))
             os.system(r'sed -i "s/SYSTEMATICS/%s/g" TMVAClassification_%s.C' %(system_unc[isys], Era)) 
             os.system(r'sed -i "s/MASS/%s/g" TMVAClassification_%s.C' %(masses[im], Era)) 
             os.system(r'sed -i "s/TMVAClassification()/TMVAClassification_%s()/g" TMVAClassification_%s.C' %(Era, Era))

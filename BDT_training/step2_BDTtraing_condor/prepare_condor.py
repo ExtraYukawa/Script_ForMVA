8e93ab8ac1ed4da80db2f70e20033833720de0bb
Gouranga Kole <gouranga.tifr@gmail.com>
2022-09-26 10:27:07 +0200

    interference sample support

--- BDT_training/step2_BDTtraing_condor/prepare_condor.py
+++ BDT_training/step2_BDTtraing_condor/prepare_condor.py
@@ -10,6 +10,8 @@ def prepare_condor(sample,coupling,mass):
   MASS=mass
   if WHICH_SAMPLE=="a":
     SIGNAL='ttc_a_'+WHICH_COUPLING+'_MA'+MASS+'.root'
+  elif WHICH_SAMPLE=="interference":
+    SIGNAL='ttc_a_'+MASS+'_s_'+str(int(MASS)-50)+'_'+WHICH_COUPLING+'.root'
   else:
     SIGNAL='ttc_s0_'+WHICH_COUPLING+'_MS0'+MASS+'.root'
 
@@ -22,6 +24,7 @@ if __name__ == "__main__":
   usage = 'usage: %prog [options]'
   parser = optparse.OptionParser(usage)
   parser.add_option('-e', '--era', dest='era', help='[all/2016apv/2016postapv/2017/2018]',default='all',type='string')
+  parser.add_option('-s', '--sampletype', dest='sampletype', help='[all/normal/interference/highmass]',default='normal',type='string')
   (args,opt) = parser.parse_args()
 
   Eras_List = ['2016postapv','2016apv','2017','2018']
@@ -30,14 +33,25 @@ if __name__ == "__main__":
     if args.era == 'all' or args.era == Era:
       Eras.append(Era)
   
-  # year="2018"
+  # Loop over Eras
   for Era in Eras:
 
     os.system('mkdir -p %s'%Era)
-    
-    signals=['a','s']
-    couplings=['rtc01','rtu01']
-    masses=['200', '700'] #,'300','350','400','500','600','700'] #fixme gkole
+    if args.sampletype == 'highmass':
+      signals=['a']
+      couplings=['rtc04','rtu04']
+      masses=['800','900','1000']
+    elif args.sampletype == 'interference':
+      signals=['interference']
+      couplings=['rtc04','rtu04']
+      #masses = ['250','300','350','400','550','700'] # asume this A mass
+      masses = ['250'] # asume this A mass
+      #S_masses = ['200','250','300','350','500','650'] # assume S mass = A-50
+    else:
+      signals=['a','s']
+      couplings=['rtc04','rtu04']
+      masses=['200','300','350','400','500','600','700'] 
+      
     system_unc=["central","jesup","jesdo","jerup","jerdo","metUnslusEnup","metUnslusEndo"]
 
     for isig in range(0,len(signals)):
@@ -46,6 +60,7 @@ if __name__ == "__main__":
           samples_temp=prepare_condor(signals[isig],couplings[ic],masses[im])
           dir_temp=samples_temp.split('.')[0].split('/')[-1]
           print ("dir_temp: ", dir_temp)
+          print ("OK------>")
           os.mkdir(Era+'/'+dir_temp)
           os.chdir(Era+'/'+dir_temp)
           print ("cwd: ", os. getcwd())
@@ -62,22 +77,35 @@ if __name__ == "__main__":
             else:
               os.system(r'cp ../../../TMVAClassification.C TMVAClassification_%s.C' %(Era))
             os.system(r'sed -i "s/SYSTEMATICS/%s/g" TMVAClassification_%s.C' %(system_unc[isys], Era)) 
-            os.system(r'sed -i "s/MASS/%s/g" TMVAClassification_%s.C' %(masses[im], Era)) 
+            
             os.system(r'sed -i "s/TMVAClassification()/TMVAClassification_%s()/g" TMVAClassification_%s.C' %(Era, Era))
             
             if signals[isig]=='a':
               os.system(r'sed -i "s/PARTICLE/a/g" wrapper_%s.sh' %(Era))
               os.system(r'sed -i "s/PARTICLE/a/g" TMVAClassification_%s.C' %(Era))
+              os.system(r'sed -i "s/MASS/%s/g" TMVAClassification_%s.C' %(masses[im], Era)) 
               os.system(r'sed -i "s/COUP/%s/g" wrapper_%s.sh' %(couplings[ic], Era))
               os.system(r'sed -i "s/COUP/%s/g" TMVAClassification_%s.C' %(couplings[ic], Era))
               os.system(r'sed -i "s/CPART/A/g" wrapper_%s.sh' %(Era))
               os.system(r'sed -i "s/MASS/%s/g" wrapper_%s.sh' %(masses[im], Era))
               os.system(r'sed -i "s/SYST/%s/g" wrapper_%s.sh' %(system_unc[isys], Era))
+            elif signals[isig]=='interference':
+              #os.system(r'sed -i "s/PARTICLE/a_s/g" wrapper_%s.sh' %(Era))
+              os.system(r'sed -i "s/PARTICLE/a_s/g" TMVAClassification_%s.C' %(Era))
+              #os.system(r'sed -i "s/COUP/%s/g" wrapper_%s.sh' %(couplings[ic], Era))
+              os.system(r'sed -i "s/COUP/%s/g" TMVAClassification_%s.C' %(couplings[ic], Era))
+              #os.system(r'sed -i "s/CPART/A_S/g" wrapper_%s.sh' %(Era))
+              #os.system(r'sed -i "s/MASS/%s/g" wrapper_%s.sh' %(masses[im]+'_'+str(int(masses[im])-50), Era))
+              
+              os.system(r'sed -i "s/MASS/%s/g" TMVAClassification_%s.C' %(masses[im]+'_'+str(int(masses[im])-50), Era))
+              os.system(r'sed -i "s/SYST/%s/g" wrapper_%s.sh' %(system_unc[isys], Era))
+              os.system(r'sed -i "s/PARTICLE_COUP_MCPARTMASS/a_%s_s_%s_%s/g" wrapper_%s.sh' %(masses[im], str(int(masses[im])-50), couplings[ic], Era))
             else:
               os.system(r'sed -i "s/PARTICLE/s0/g" wrapper_%s.sh' %(Era))
               os.system(r'sed -i "s/PARTICLE/s0/g" TMVAClassification_%s.C' %(Era))
               os.system(r'sed -i "s/COUP/%s/g" wrapper_%s.sh' %(couplings[ic], Era))
               os.system(r'sed -i "s/COUP/%s/g" TMVAClassification_%s.C' %(couplings[ic], Era))
+              os.system(r'sed -i "s/MASS/%s/g" TMVAClassification_%s.C' %(masses[im], Era)) 
               os.system(r'sed -i "s/CPART/S0/g" wrapper_%s.sh' %(Era))
               os.system(r'sed -i "s/MASS/%s/g" wrapper_%s.sh' %(masses[im], Era))
               os.system(r'sed -i "s/SYST/%s/g" wrapper_%s.sh' %(system_unc[isys], Era))
@@ -94,6 +122,12 @@ if __name__ == "__main__":
             os.system(r'sed -i "s/COUP/%s/g" sub.jdl' %(couplings[ic]))
             os.system(r'sed -i "s/CPART/A/g" sub.jdl')
             os.system(r'sed -i "s/MASS/%s/g" sub.jdl' %(masses[im]))
+          elif signals[isig]=='interference':
+            #os.system(r'sed -i "s/PARTICLE/a_s/g" sub.jdl')
+            #os.system(r'sed -i "s/COUP/%s/g" sub.jdl' %(couplings[ic]))
+            #os.system(r'sed -i "s/CPART/A_S/g" sub.jdl')
+            #os.system(r'sed -i "s/MASS/%s/g" sub.jdl' %(masses[im]))
+            os.system(r'sed -i "s/ttc_PARTICLE_COUP_MCPARTMASS/%s/g" sub.jdl' %(dir_temp))
           if signals[isig]=='s':
             os.system(r'sed -i "s/PARTICLE/s0/g" sub.jdl')
             os.system(r'sed -i "s/COUP/%s/g" sub.jdl' %(couplings[ic]))

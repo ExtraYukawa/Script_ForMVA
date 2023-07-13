cac8ac1f4266442573ac5a98e61ea011f01b5d8b
AlbertHsuNTUphys <b07202037@ntu.edu.tw>
2023-07-13 09:49:31 +0200

    [DNN] Update DNN application

--- python/DNN_application.py
+++ python/DNN_application.py
@@ -13,6 +13,54 @@ def Build_DNN_Command(model_name="", df=None):
   std_str = [str(x) for x in std]
   mean_array_str = '{' + ', '.join(mean_str) + '}'
   std_array_str = '{' + ', '.join(std_str) + '}'
+  var_dict = {'bmatched_jet_pt':'jet_pt[comb_[0]]',
+              'lmatched_jet_pt':'jet_pt[comb_[1]]',
+              'jet3_pt': 'jet_pt[comb_[2]]',
+              'jet4_pt': '(comb_[3] > -1) ? jet_pt[comb_[3]] : -9.0',
+              'bmatched_jet_eta':'jet_eta[comb_[0]]',
+              'lmatched_jet_eta':'jet_eta[comb_[1]]',
+              'jet3_eta': 'jet_eta[comb_[2]]',
+              'jet4_eta': '(comb_[3] > -1) ? jet_eta[comb_[3]] : -9.0',
+              'bmatched_jet_phi':'jet_phi[comb_[0]]',
+              'lmatched_jet_phi':'jet_phi[comb_[1]]',
+              'jet3_phi': 'jet_phi[comb_[2]]',
+              'jet4_phi': '(comb_[3] > -1) ? jet_phi[comb_[3]] : -9.0',
+              'bmatched_jet_mass':'jet_mass[comb_[0]]',
+              'lmatched_jet_mass':'jet_mass[comb_[1]]',
+              'jet3_mass': 'jet_mass[comb_[2]]',
+              'jet4_mass': '(comb_[3] > -1) ? jet_mass[comb_[3]] : -9.0',
+              'bmatched_jet_CvB': 'jet_CvB[comb_[0]]',
+              'lmatched_jet_CvB': 'jet_CvB[comb_[1]]',
+              'jet3_CvB': 'jet_CvB[comb_[2]]',
+              'jet4_CvB': '(comb_[3] > -1) ? jet_CvB[comb_[3]] : -9.0',
+              'bmatched_jet_CvL': 'jet_CvL[comb_[0]]',
+              'lmatched_jet_CvL': 'jet_CvL[comb_[1]]',
+              'jet3_CvL': 'jet_CvL[comb_[2]]',
+              'jet4_CvL': '(comb_[3] > -1) ? jet_CvL[comb_[3]] : -9.0',
+              'bmatched_jet_FlavB': 'jet_FlavB[comb_[0]]',
+              'lmatched_jet_FlavB': 'jet_FlavB[comb_[1]]',
+              'jet3_FlavB': 'jet_FlavB[comb_[2]]',
+              'jet4_FlavB': '(comb_[3] > -1) ? jet_FlavB[comb_[3]] : -9.0',
+              'lep1_pt': 'l1_pt',
+              'lep2_pt': 'l2_pt',
+              'lep1_eta': 'l1_eta',
+              'lep2_eta': 'l2_eta',
+              'lep1_phi': 'l1_phi',
+              'lep2_phi': 'l2_phi',
+              'lep1_mass': 'l1_mass',
+              'lep2_mass': 'l2_mass',
+              'dR_bmatched_lmatched_jets': 'ROOT::Math::VectorUtil::DeltaR(j1,j2)',
+              'dR_bmatched_jet_lep1': 'ROOT::Math::VectorUtil::DeltaR(j1,l1)',
+              'dR_bmatched_jet_lep2': 'ROOT::Math::VectorUtil::DeltaR(j1,l2)',
+              'dR_lmatched_jet_lep1': 'ROOT::Math::VectorUtil::DeltaR(j2,l1)',
+              'dR_lmatched_jet_lep2': 'ROOT::Math::VectorUtil::DeltaR(j2,l2)',
+              'invmass_bjlj': '(j1+j2).M()',
+              'met':'met',
+              'met_phi':'met_phi'
+               }
+  input_def = ''
+  for idx,col_name in enumerate(col_names):
+    input_def += 'input[{idx}] = {def_};\n'.format(idx=idx,def_=var_dict[col_name])
   command = '\
   #include "ROOT/RDataFrame.hxx"\n\
   #include "ROOT/RVec.hxx"\n\
@@ -33,17 +81,38 @@ def Build_DNN_Command(model_name="", df=None):
                                ROOT::RVec<Float_t> jet_CvL, \n\
                                ROOT::RVec<Float_t> jet_CvB, \n\
                                ROOT::RVec<Float_t> jet_FlavB, \n\
-                               float l1_pt, \n\
-                               float l1_eta, \n\
-                               float l1_phi, \n\
-                               float l1_mass, \n\
-                               float l2_pt, \n\
-                               float l2_eta, \n\
-                               float l2_phi, \n\
-                               float l2_mass, \n\
+                               float ttc_l1_pt, \n\
+                               float ttc_l1_eta, \n\
+                               float ttc_l1_phi, \n\
+                               float ttc_l1_mass, \n\
+                               float ttc_l2_pt, \n\
+                               float ttc_l2_eta, \n\
+                               float ttc_l2_phi, \n\
+                               float ttc_l2_mass, \n\
                                float met, \n\
                                float met_phi \n\
                                ){{ \n\
+  float l1_pt, l1_eta, l1_phi, l1_mass, l2_pt, l2_eta, l2_phi, l2_mass;\n\
+  if(ttc_l1_pt < ttc_l2_pt){{\n\
+    l1_pt  = ttc_l2_pt;\n\
+    l1_eta = ttc_l2_eta;\n\
+    l1_phi = ttc_l2_phi;\n\
+    l1_mass= ttc_l2_mass;\n\
+    l2_pt  = ttc_l1_pt;\n\
+    l2_eta = ttc_l1_eta;\n\
+    l2_phi = ttc_l1_phi;\n\
+    l2_mass= ttc_l1_mass;\n\
+  }}\n\
+  else{{\n\
+    l1_pt  = ttc_l1_pt;\n\
+    l1_eta = ttc_l1_eta;\n\
+    l1_phi = ttc_l1_phi;\n\
+    l1_mass= ttc_l1_mass;\n\
+    l2_pt  = ttc_l2_pt;\n\
+    l2_eta = ttc_l2_eta;\n\
+    l2_phi = ttc_l2_phi;\n\
+    l2_mass= ttc_l2_mass;\n\
+  }}\n\
   int njet = std::min(6,static_cast<int>(jet_id.size()));\n\
   std::vector<std::vector<int>> combination;\n\
   for(int first_idx = 0; first_idx < njet; first_idx++){{ \n\
@@ -68,66 +137,12 @@ def Build_DNN_Command(model_name="", df=None):
     std::vector<int> comb_; \n\
     comb_ = combination[comb_idx];\n\
     float input[{input_dim}];\n\
-    input[0] = jet_pt[comb_[0]];\n\
-    input[1] = jet_eta[comb_[0]];\n\
-    input[2] = jet_phi[comb_[0]];\n\
-    input[3] = jet_mass[comb_[0]];\n\
-    input[4] = jet_CvB[comb_[0]];\n\
-    input[5] = jet_CvL[comb_[0]];\n\
-    input[6] = jet_FlavB[comb_[0]];\n\
-    input[7] = jet_pt[comb_[1]];\n\
-    input[8] = jet_eta[comb_[1]];\n\
-    input[9] = jet_phi[comb_[1]];\n\
-    input[10]= jet_mass[comb_[1]];\n\
-    input[11]= jet_CvB[comb_[1]];\n\
-    input[12]= jet_CvL[comb_[1]];\n\
-    input[13]= jet_FlavB[comb_[1]];\n\
     ROOT::Math::PtEtaPhiMVector j1(jet_pt[comb_[0]],jet_eta[comb_[0]],jet_phi[comb_[0]],jet_mass[comb_[0]]);\n\
     ROOT::Math::PtEtaPhiMVector j2(jet_pt[comb_[1]],jet_eta[comb_[1]],jet_phi[comb_[1]],jet_mass[comb_[1]]);\n\
     ROOT::Math::PtEtaPhiMVector j3(jet_pt[comb_[2]],jet_eta[comb_[2]],jet_phi[comb_[2]],jet_mass[comb_[2]]);\n\
     ROOT::Math::PtEtaPhiMVector l1(l1_pt,l1_eta,l1_phi,l1_mass);\n\
     ROOT::Math::PtEtaPhiMVector l2(l2_pt,l2_eta,l2_phi,l2_mass);\n\
-    input[14] = ROOT::Math::VectorUtil::DeltaR(j1,j2);\n\
-    input[15] = ROOT::Math::VectorUtil::DeltaR(j1,l1);\n\
-    input[16] = ROOT::Math::VectorUtil::DeltaR(j1,l2);\n\
-    input[17] = ROOT::Math::VectorUtil::DeltaR(j2,l1);\n\
-    input[18] = ROOT::Math::VectorUtil::DeltaR(j2,l2);\n\
-    input[19] = (j1+j2).M();\n\
-    input[20] = l1_pt;\n\
-    input[21] = l1_eta;\n\
-    input[22] = l1_phi;\n\
-    input[23] = l1_mass;\n\
-    input[24] = l2_pt;\n\
-    input[25] = l2_eta;\n\
-    input[26] = l2_phi;\n\
-    input[27] = l2_mass;\n\
-    input[28] = jet_pt[comb_[2]];\n\
-    input[29] = jet_eta[comb_[2]];\n\
-    input[30] = jet_phi[comb_[2]];\n\
-    input[31] = jet_mass[comb_[2]];\n\
-    input[32] = jet_CvB[comb_[2]];\n\
-    input[33] = jet_CvL[comb_[2]];\n\
-    input[34] = jet_FlavB[comb_[2]];\n\
-    if(comb_[3] > -1){{\n\
-      input[35] = jet_pt[comb_[3]];\n\
-      input[36] = jet_eta[comb_[3]];\n\
-      input[37] = jet_phi[comb_[3]];\n\
-      input[38] = jet_mass[comb_[3]];\n\
-      input[39] = jet_CvB[comb_[3]];\n\
-      input[40] = jet_CvL[comb_[3]];\n\
-      input[41] = jet_FlavB[comb_[3]];\n\
-    }}\n\
-    else{{\n\
-      input[35] = -9.0;\n\
-      input[36] = -9.0;\n\
-      input[37] = -9.0;\n\
-      input[38] = -9.0;\n\
-      input[39] = -9.0;\n\
-      input[40] = -9.0;\n\
-      input[41] = -9.0;\n\
-    }}\n\
-    input[42] = met;\n\
-    input[43] = met_phi;\n\
+    {input_def}\n\
     for(int input_idx; input_idx < {input_dim}; input_idx++){{\n\
       input[input_idx] = (input[input_idx]-Preprocessor_mean[input_idx])/Preprocessor_std[input_idx];\n\
     }}\n\
@@ -139,8 +154,8 @@ def Build_DNN_Command(model_name="", df=None):
   }}\n\
   return best_index;\n\
  }}\
-'.format(input_dim = len(col_names), model_name=model_name, mean_array = mean_array_str, std_array=std_array_str)
-  #print(command)
+'.format(input_dim = len(col_names), model_name=model_name, mean_array = mean_array_str, std_array=std_array_str, input_def=input_def)
+  print(command)
   ROOT.gInterpreter.Declare(str(command))
   if df:
     df = df.Define('JetMatched_idx','Jet_reindex(tightJets_id_in24,\
@@ -162,3 +177,6 @@ def Build_DNN_Command(model_name="", df=None):
                                          ttc_met,\
                                          ttc_met_phi)')
   return df
+
+if __name__ == '__main__':
+  Build_DNN_Command(model_name="Flav_MET_threeJet_model_v2", df=None)
